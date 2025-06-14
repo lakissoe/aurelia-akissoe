2f1a5a8742e09265308e9fb9e6e50cb20c75b56c
Lorène AKISSOE <lorene.akissoe@ird.fr>
2025-06-14 11:59:41 +0200

    update

--- projects/script.js
+++ projects/script.js
@@ -55,7 +55,8 @@ function showProjects(projects) {
         <div class="desc">
           <p>${project.desc}</p>
           <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>,
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> En savoir plus</a>
+
           </div>
         </div>
       </div>
