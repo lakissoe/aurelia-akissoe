99ef0aaac7f43273971889aad6ac8f53032568d8
Lorène AKISSOE <lorene.akissoe@ird.fr>
2025-11-02 21:33:16 +0100

    Initial commit - Nouveau portfolio

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Lorène AKISSOE";
+            document.title = "Projects | Portfolio Aurélia AKISSOE";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
